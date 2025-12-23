53b867756a68f9891a284ea41e39765c4710abb9
uni-parser <227558684+uni-parser@users.noreply.github.com>
2025-12-23 03:37:49 +0000

    fix: update commercial contact email

--- src/components/sections/CustomFooter.vue
+++ src/components/sections/CustomFooter.vue
@@ -35,7 +35,7 @@ const links = [
           </div>
           <div class="contact-info">
             <p class="contact-text">{{ t('custom_footer.email_contact') }}:</p>
-            <a href="mailto:mengyue@dp.tech" class="email-link">{{ t('custom_footer.commercial_contact') }}: mengyue@dp.tech</a>
+            <a href="mailto:bd@dp.tech" class="email-link">{{ t('custom_footer.commercial_contact') }}: bd@dp.tech</a>
             <a href="mailto:fangxi@dp.tech" class="email-link">{{ t('custom_footer.academic_contact') }}: fangxi@dp.tech</a>
           </div>
         </div>
